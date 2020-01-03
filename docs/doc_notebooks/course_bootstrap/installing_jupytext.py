12b1c19ea9733daa9359927213ccf7ff59709be7
Phil Austin <paustin@eos.ubc.ca>
2020-01-03 11:42:40 -0800

    namechange

---
+++ docs/doc_notebooks/course_bootstrap/installing_jupytext.py
@@ -0,0 +1,30 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all
+#     formats: ipynb,py:percent
+#     notebook_metadata_filter: all,-language_info,-toc,-latex_envs
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.3.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Using jupytext
+#
+# ## Basic workflow
+#
+# 1) always launch the py version of the notebook, treat the ipynb file as
+#    readonly storage
+#    
+# 2) always commit the py version to git, never the ipynb version
+#
+# 3) ...
+
+# %%
