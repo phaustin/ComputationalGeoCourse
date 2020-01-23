76e7fe8b0c407657f2a3964009c8a22726d336df
Keegan Lensink <keeganlensink@gmail.com>
2020-01-23 12:59:39 -0800

    rename A1 to E1

--- students/assignments/assignment1/mainE1.py
+++ students/assignments/assignment1/mainE1.py
@@ -1,8 +1,9 @@
-# EOSC213 Assignment 1 Exercise 1
+# EOSC213 Assignment 1.1
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
 plt.ion()
+
 
 def misfit(a,b):
     """Computes the distance between a and b. We will use this to compare how close
@@ -20,46 +21,77 @@ def misfit(a,b):
 
 
 def getGradients(T,x,y):
+    """ Compute dT/dx and dT/dy using finite difference methods.
+    
+    Arguments:
+        T {torch.Tensor} -- A 2-dimensional tensor of data measured on a grid.
+        x {torch.Tensor} -- 1-dimensional tensor of x grid point locations.
+        y {torch.Tensor} -- 1-dimensional tensor of y grid point locations.
+    
+    Returns:
+        [torch.Tensor] -- dT/dx
+        [torch.Tensor] -- dT/dy
+    """
 
+    # These are place holders, you will overwrite them in your code.
     dummy = torch.zeros(3,4)
     Tx = dummy
     Ty = dummy
-    # your code here to compute Tx, Ty
+
+    # TODO: your code here to compute Tx, Ty
     
     return Tx, Ty
 
+
 def getAbsGrad(Tx,Ty):
+    """ Compute the partials Tx and Ty, compute the absolute value of the gradients.
     
+    Arguments:
+        Tx {torch.Tensor} -- dT/dx
+        Ty {torch.Tensor} -- dT/dy
+    
+    Returns:
+        [torch.Tensor] -- |\nabla T|
+    """
+    
+    # These are place holders, you will overwrite them in your code.
     dummy = torch.zeros(3,4)
     absGradT = dummy
-    # your code here to compute absGradT = sqrt(Tx^2 + Ty^2)
+
+    # TODO: your code here to compute absGradT = sqrt(Tx^2 + Ty^2)
     
     return absGradT
 
 
 if __name__ == "__main__":
+
+    # In this exercise you are going to use finite difference methods to compute
+    # the absolute values of the gradient of a topographic map. In the assignment 
+    # directory there are file 'x.pt', 'y.pt', and 'topo.pt' that contain torch.Tensors.
+    # topo.pt contains data that was measured on a grid, as described by the grid points 
+    # in 'x.pt' and 'y.pt'
      
-    # load topo file
+    # TODO: Finish loading y and topo here
+    # An example of how to load a torch tensor
     x = torch.load('x.pt')
-    # load y and topo here
-    #
-    #
+    # y = 
+    # topo = 
     
+    # Uncomment these lines to plot the data, you should do this at least once!
     #plt.imshow(topo, extent=[x.min(), x.max(), y.min(), y.max()])
     #plt.show()
     
     # Compute the gradient
     Tx, Ty =  getGradients(topo,x,y)
     
-    
     # Compute |\grad T|
     absGradT = getAbsGrad(Tx,Ty)
 
-
-    # load correct answer
+    # TODO: load correct answer
     #absGradTtrue = 
     
-    # compare
+    # Here we are going to measure the distance between your AbsGrad and the true answer.
+    # Don't change epsilon here.
     epsilon = 0.001
     pss = 0
     if misfit(absGradT,absGradTtrue)<epsilon:
